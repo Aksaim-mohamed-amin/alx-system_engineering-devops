841d2d0c8d5f3836ec5d2495d4817231695d8109
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-21 07:53:34 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 # install flask from pip3
-package { 'python3-pip':
-  ensure => installed
-}
-
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip']
 }
