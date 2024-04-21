8fac87a7258b51a3a573a92fc9e51252ae175502
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-21 08:40:28 -0700

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 
 include stdlib
 
-file_line { 'Turn off password authontication':
+file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/etc/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => present,
-  path   => '/etc/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
