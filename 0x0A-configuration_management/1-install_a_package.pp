2d2e78f95b798ee6a3c2ed04a3be923a18468faa
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-05-04 06:06:55 -0700

    task 1: Install flask 2.1.0 using puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# install flask from pip3
+
+package {'python3':
+  ensure => installed,
+}
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3'],
+}
