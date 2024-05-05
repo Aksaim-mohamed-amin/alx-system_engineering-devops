73f19e45b7143e532574c33446a298470fae5a64
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-05-05 16:48:20 -0700

    task 2: Confugure Nginx server with a custom header using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,12 +15,27 @@ service { 'nginx':
 # Define a custom index page
 file { '/var/www/html/index.html':
   ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
   content => 'Hello World!',
+}
+
+# Define a custom 404 page
+file { '/var/www/html/404.html':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  content => "Ceci n'est pas une page"
 }
 
 # Configure Nginx server
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
   content => '
 server {
        listen 80 default_server;
@@ -34,6 +49,16 @@ server {
        location / {
                 try_files $uri $uri/ =404;
                 add_header X-Served-By $hostname;
+       }
+       
+       location /redirect_me {
+                return 301 https://youtube.com;
+       }
+
+       error_page 404 /404.html;
+       location = /404.html {
+                root /var/www/html;
+                internal;
        }
 }',
   require => Package['nginx'],
