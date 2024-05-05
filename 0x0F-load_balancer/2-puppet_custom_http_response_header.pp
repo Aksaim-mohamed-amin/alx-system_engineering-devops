db5c9b6d4ba5693bc2832077a788df9dd557fb60
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-05-05 16:16:40 -0700

    task 2: Confugure Nginx server with a custom header using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,45 +1,66 @@
-# Configure Nginx server.
+# Install Nginx web server and configure it
 
-# Update package lists
-exec { 'apt_update':
-  command => 'apt-get update',
-}
-
-# Install Nginx
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
-  require => Exec['apt_update'],
 }
 
-# Allow connections to Nginx over the firewall
-ufw::allow { 'Nginx HTTP':
-  port => '80',
-}
-
-# Create the index.html file
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
-
-# Create a custom 404 error page
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-}
-
-# Configure Nginx to listen on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx server
+# Ensure Nginx service is enabled and running
 service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
-  subscribe => File['/etc/nginx/sites-available/default'],
+}
+
+# Define a custom index page
+file { '/var/www/html/index.html':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  content => 'Hello World!',
+}
+
+# Define a custom 404 page
+file { '/var/www/html/404.html':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  content => "Ceci n'est pas une page"
+}
+
+# Configure Nginx server
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  content => '
+server {
+       listen 80 default_server;
+       listen [::]:80 default_server;
+
+       root /var/www/html;
+       index index.html;
+
+       server_name _;
+
+       location / {
+                try_files $uri $uri/ =404;
+                add_header X-Served-By ${hostname};
+       }
+       
+       location /redirect_me {
+                return 301 https://youtube.com;
+       }
+
+       error_page 404 /404.html;
+       location = /404.html {
+                root /var/www/html;
+                internal;
+       }
+}',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
