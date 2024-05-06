60bee1b36d149b539d6dc2aeea6e716c71821577
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-05-05 17:01:59 -0700

    ttttkjhgqlksgfmjn

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,24 @@
-#puppet advance
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
+# Install Nginx web server and configure it with a custom header                          
+
+# Install Nginx package                                                                    
+package { 'nginx':
+  ensure => installed,
 }
--> package {'nginx':
-  ensure => present,
+
+# Ensure Nginx service is enabled and running                                             
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
--> file_line { 'header line':
+
+# Configure Nginx server                                                                  
+file_line { 'add the custom header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
+  line   => "                                                                             
+  location / {                                                                            
+    add_header X-Served-By ${hostname};",
   match  => '^\tlocation / {',
-}
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+  notify => Service['nginx'],
 }
