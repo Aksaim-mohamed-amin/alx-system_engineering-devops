e4d2ad44d0bd154da2baddfbb9d1cb393668211e
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-07-08 09:01:27 -0700

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# make changes to the ssh configuration file
+file { '/home/user/.ssh/config':
+  ensure => present,
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/home/user/.ssh/config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/home/user/.ssh/config',
+  lin    => 'IdentityFile ~/.ssh/school',
+}
