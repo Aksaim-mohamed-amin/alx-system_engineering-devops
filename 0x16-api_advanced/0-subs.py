2102aaf34641acfbcdd09b4838432061548e38ab
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-08-13 09:40:18 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ALX-APP'}
 
-    rs = requests.get(url, headers=headers)
+    rs = requests.get(url, headers=headers, allow_redirects=False)
 
     print(rs.status_code)
 
