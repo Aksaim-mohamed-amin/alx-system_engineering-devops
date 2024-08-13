9f348c6b75144e20bd60779edbd86fff456fe194
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-08-13 09:39:30 -0700

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'ALX-APP'}
+
+    rs = requests.get(url, headers=headers)
+
+    print(rs.status_code)
+
+    if rs.status_code == 200:
+        return rs.json()['data']['subscribers']
+    else:
+        return 0
