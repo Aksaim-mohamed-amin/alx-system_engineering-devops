e0540cce81c5190d4540ad6e9c768c1bbc261edc
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-10 03:33:19 -0700

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,25 +1,15 @@
-# Ensure the ssh_config file exists
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
+# make changes to config file using Puppet
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
-# Ensure PasswordAuthentication is set to 'no'
-file_line { 'Turn off passwd auth':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  append_on_no_match => true
-}
-
-# Ensure IdentityFile is set to the correct key
-file_line { 'Declare identity file':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  append_on_no_match => true
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
